71e3ed4efcc1c31ea936fa912c9b0b14be5b2426
Eiji Onchi <onchi.eiji@gmail.com>
2018-10-19 10:55:11 +0900

    Added bash script to download books

--- .scripts/books.sh
+++ .scripts/books.sh
@@ -1,33 +1,38 @@
-#!/bin/zsh
+#!/bin/bash
 
 main() {
     echo "Waking up monkeys to look for: $@"
 
-    page=$(curl -s 'http://libgen.io/search.php?req='$(echo "$@" | sed 's/ /+/g')'&open=0&res=25&view=simple&phrase=1&column=def')
+    page="$(curl -s 'http://libgen.io/search.php?req='$(echo "$@" | sed 's/ /+/g')'&open=0&res=25&view=simple&phrase=1&column=def')"
 
-    titles=$(echo $page | grep -o "title='' id=[^<]*" | sed 's/.*>//g;
-    s/$/                                            /g' | cut -c -44 | sed -e 's/$/.../g')
+    titles=$(echo "$page" | grep -o "title='' id=[^<]*" | sed 's/.*>//g; s/$/                            /g' | cut -c -29 | sed -e 's/$/ \|/g')
 
     [[ 1 != $(echo "$titles" | wc --chars) ]] || (echo "Nothing found!" && exit)
 
-    sizes=$(echo $page | grep -o ">.*MB\|>.*kB" | tr -d '>')
-    exts=$(echo $page | grep -A1 ">.*MB\|>.*kB" | grep -v ">.*MB\|>.*kB\|--" | grep -o ">.*<" | tr -d '><')
-    files=($(echo $page | grep 'a href' | grep -o "libgen.io/ads[^\']*") )
+    authors=$(  echo "$page" | grep -o "=author'>[^ ][^<]*" | sed 's/.*>//g; s/$/            /g' | cut -c -13 | sed -e 's/$/ \|/g')
+    years=$(    echo "$page" | grep -o "nowrap>[[:xdigit:]]\{,4\}<" | sed 's/.*>//g; s/<//g; s/$/     /g' | cut -c -4 | sed 's/$/ \|/g')
+    exts=$(     echo "$page" | grep -A1 ">[[:xdigit:]]* [[:alpha:]]\{,1\}B" | grep -v ">.*B\|--" | grep -o ">.*<" | tr -d '><')
+    sizes=$(    echo "$page" | grep -o ">[[:xdigit:]]* [[:alpha:]]\{,1\}B" | tr -d '>' | xargs -d '\n' printf '%6s\n')
 
-    labels=$(paste <(echo "$titles") <(echo "$sizes") <(echo "$exts"))
+    files=($(echo "$page" | grep 'a href' | grep -o "libgen.io/ads[^\']*") )
+
+    labels=$(paste <(echo "") <(echo "$titles") <(echo "$authors") <(echo "$years") <(echo "$exts") <(echo "$sizes"))
 
     IFS=$'\n'
     names=($(echo "$labels"))
 
-    PS3="Select a file to download: "
+    PS3="Write the file ID to download: "
 
     COLUMNS=12
     echo "Files found:"
-    select option in "${names[@]}" "exit";
+    echo
+    echo -e "ID    \tTitle                        \tAuthor       \tYear\tType  \t  Size"
+    echo -e "======\t=============================\t=============\t====\t======\t======"
+    select option in "${names[@]}" "*** SELECT THIS TO EXIT ***";
     do
         case $option in
-            "exit")
-                echo Cancelled
+            "*** SELECT THIS TO EXIT ***")
+                echo "The monkeys went to sleep..."
                 break ;;
             *)
                 echo 
