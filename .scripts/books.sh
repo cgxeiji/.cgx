a12bf70c5993d54b33a75642dbe24986b5ec08f6
Eiji Onchi <onchi.eiji@gmail.com>
2018-10-20 17:17:24 +0900

    Fixed printing artifacts

--- .scripts/books.sh
+++ .scripts/books.sh
@@ -5,17 +5,38 @@ main() {
 
     page="$(curl -s 'http://libgen.io/search.php?req='$(echo "$@" | sed 's/ /+/g')'&open=0&res=25&view=simple&phrase=1&column=def')"
 
-    titles=$(echo "$page" | grep -o "title='' id=[^<]*" | sed 's/.*>//g; s/$/                            /g' | cut -c -29 | sed -e 's/$/ \|/g')
+    titles=$(echo "$page" |
+        grep -o "title='' id=[^<]*" |
+        tr -d '\200-\377' | 
+        sed 's/.*>//g; s/$/                            /g' |
+        cut -c -29 |
+        sed -e 's/$/ \|/g')
 
     [[ 1 != $(echo "$titles" | wc --chars) ]] || {
         echo "The monkeys slipped on a banana and found nothing!"
         exit
     }
 
-    authors=$(  echo "$page" | grep -o "=author'>[^ ][^<]*" | sed 's/.*>//g; s/$/            /g' | cut -c -13 | sed -e 's/$/ \|/g')
-    years=$(    echo "$page" | grep -o "nowrap>[[:xdigit:]]\{,4\}<" | sed 's/.*>//g; s/<//g; s/$/     /g' | cut -c -4 | sed 's/$/ \|/g')
-    exts=$(     echo "$page" | grep -A1 ">[[:xdigit:]]* [[:alpha:]]\{,1\}B" | grep -v ">.*B\|--" | grep -o ">.*<" | tr -d '><')
-    sizes=$(    echo "$page" | grep -o ">[[:xdigit:]]* [[:alpha:]]\{,1\}B" | tr -d '>' | xargs -d '\n' printf '%6s\n')
+    authors=$(echo "$page" |
+        grep -o "=author'>[^ ][^<]*" |
+        tr -d '\200-\377' | 
+        sed 's/.*>//g; s/$/             /g' |
+        cut -c -13 |
+        sed -e 's/$/ \|/g')
+    years=$(echo "$page" |
+        grep -o "nowrap>[[:xdigit:]]\{,4\}<" |
+        sed 's/.*>//g; s/<//g; s/$/     /g' |
+        cut -c -4 |
+        sed 's/$/ \|/g')
+    exts=$(echo "$page" |
+        grep -A1 ">[[:xdigit:]]* [[:alpha:]]\{1\}B" |
+        grep -v ">.*B\|--" |
+        grep -o ">.*<" |
+        tr -d '><')
+    sizes=$(echo "$page" |
+        grep -o ">[[:xdigit:]]* [[:alpha:]]\{1\}B" |
+        tr -d '>' |
+        xargs -d '\n' printf '%6s\n')
 
     files=($(echo "$page" | grep 'a href' | grep -o "libgen.io/ads[^\']*") )
 
@@ -24,19 +45,21 @@ main() {
     IFS=$'\n'
     names=($(echo "$labels"))
 
-    PS3="Write the file ID to download: "
+    PS3=$'\n'"Write the file ID to download (0 to exit): "
 
     COLUMNS=12
+    echo
     echo "Files found:"
     echo
     echo -e "ID    \tTitle                        \tAuthor       \tYear\tType  \t  Size"
     echo -e "======\t=============================\t=============\t====\t======\t======"
-    select option in "${names[@]}" "*** SELECT THIS TO EXIT ***";
+    select option in "${names[@]}"
     do
+        [[ $REPLY -eq 0 ]] && {
+            echo "The monkeys went to sleep..."
+            break
+        }
         case $option in
-            "*** SELECT THIS TO EXIT ***")
-                echo "The monkeys went to sleep..."
-                break ;;
             *)
                 echo 
                 echo "Target locked on: $option"
--- .scripts/papers.sh
+++ .scripts/papers.sh
@@ -4,13 +4,37 @@ main() {
     echo "Waking up monkeys to look for: $@"
 
     page="$(curl -s 'https://search.crossref.org/?q='$(echo "$@" | sed 's/ /+/g'))"
-    #echo "$page" | tr -d '\n' | grep -o "class='item-data'[^%]*" | grep -o "Authors:[^<]*\|title\|item-data" 
-    #| sed -n '/lead/{n;p;}' | sed 's/ctx//g; s/Authors: //g'
-    #echo "$page" | tr -d '\n' | grep -o "class='cite-link'[^>]*" | grep -o ", '[^']*" | sed -e 's/$/                                        /g' | cut -c 4-40 | sed 's/$/ \|/g'
+    
+    ilinks=$(echo "$page" |
+        grep -o "class='cite-link'[^>]*" |
+        grep -o ".org/[^']*" |
+        cut -c 6-)
 
-    links=("" $(echo "$page" | grep -o "class='cite-link'[^>]*" | grep -o ".org/[^']*" | cut -c 6-))
-    titles=$(echo "$page" | grep -o "class='cite-link'[^>]*" | grep -o ", '[^']*" | sed "s/, '//g; s/$/                                        /g" | cut -c -37 | sed 's/$/ \|/g')
-    authors=$(echo "$page" | tr -d '\n' | grep -o "class='item-data'[^%]*" | grep -o "Authors:[^<]*\|title\|item-data" | sed -n '/item-data/{n;p;}' | sed 's/title//g; s/Authors: //g' | sed 's/$/             /g' | cut -c -13 | sed 's/$/ \|/g' )
+    [[ 1 != $(echo "$ilinks" | wc --chars) ]] || {
+        echo "The monkeys slipped on a banana and found nothing!"
+        exit
+    }
+
+    links=("" $ilinks)
+    ilinks=$(echo "$ilinks" |
+        cut -c 8-21)
+    titles=$(echo "$page" | grep -o "class='cite-link'[^>]*" | 
+        grep -o ", '[^']*" | 
+        tr -d '\200-\377' | 
+        sed "s/, '//g; s/$/                                        /g" |
+        cut -c -37 |
+        sed 's/$/ \|/g')
+    authors=$(echo "$page" |
+        tr -d '\n' |
+        grep -o "class='item-data'[^%]*" |
+        sed 's/<span class="hl">//g; s/<\/span>//g' |
+        grep -o "Authors:[^<]*\|title\|item-data" |
+        sed -n '/item-data/{n;p;}' |
+        sed 's/title//g; s/Authors: //g' |
+        tr -d '\200-\377' | 
+        sed 's/$/             /g' |
+        cut -c -13 |
+        sed 's/$/ \|/g' )
 
     [[ 1 != $(echo "$titles" | wc --chars) ]] || {
         echo "The monkeys slipped on a banana and found nothing!"
@@ -18,28 +42,29 @@ main() {
     }
 
 
-    labels=$(paste <(echo "") <(echo "$titles") <(echo "$authors"))
+    labels=$(paste <(echo "") <(echo "$titles") <(echo "$authors") <(echo "$ilinks"))
 
     IFS=$'\n'
     names=($(echo "$labels"))
 
-    PS3="Write the file ID to download: "
+    PS3=$'\n'"Write the file ID to download (0 to exit): "
 
     COLUMNS=12
+    echo
     echo "Files found:"
     echo
-    echo -e "ID    \tTitle                                \tAuthor"
-    echo -e "======\t=====================================\t============="
-    select option in "${names[@]}" "*** SELECT THIS TO EXIT ***";
+    echo -e "ID    \tTitle                                \tAuthor       \tDOI"
+    echo -e "======\t=====================================\t=============\t=============="
+    select option in "${names[@]}"
     do
+        [[ $REPLY -eq 0 ]] && {
+            echo "The monkeys went to sleep..."
+            break
+        }
         case $option in
-            "*** SELECT THIS TO EXIT ***")
-                echo "The monkeys went to sleep..."
-                break ;;
             *)
                 echo 
                 echo "Target locked on: $option"
-                echo "selected: $REPLY"
                 echo "Deploying bots to look for the file ""${links[$REPLY]}"
                 curl -f -w 'Got: %{filename_effective}\n' -# -O $(curl -s 'http://sci-hub.tw/'"${links[$REPLY]}" | grep location.href | grep -o 'http.*pdf') || {
                     echo "The bots went to Mordor and found nothing!"
