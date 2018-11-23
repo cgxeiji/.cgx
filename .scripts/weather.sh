2fa34e7f09e9f86ad1dd3ef1b8084179d01ebf84
Eiji Onchi <onchi.eiji@gmail.com>
2018-11-23 16:17:58 +0900

    fixed weather skipping icons

--- .scripts/weather.sh
+++ .scripts/weather.sh
@@ -21,18 +21,18 @@ rain=$(
 # Icon Table using Weather Icons
 [[ $(date +%H) < 18 ]] && {
     # Daytime
-    [[ "$cast" =~ "Clear" ]] && cast=$(echo -n ) ||
-    [[ "$cast" =~ "Sunny" ]] && cast=$(echo -n ) ||
-    [[ "$cast" =~ "Partly cloudy" ]] && cast=$(echo -n ) ||
-    [[ "$cast" =~ "Cloudy" ]] && cast=$(echo -n ) ||
-    [[ "$cast" =~ "Shallow Fog" ]] && cast=$(echo -n )
+    [[ "$cast" =~ "Clear" ]] && cast=$(echo -n ) 
+    [[ "$cast" =~ "Sunny" ]] && cast=$(echo -n ) 
+    [[ "$cast" =~ "Partly cloudy" ]] && cast=$(echo -n ) 
+    [[ "$cast" =~ "Cloudy" ]] && cast=$(echo -n ) 
+    [[ "$cast" =~ "Shallow Fog" ]] && cast=$(echo -n ) 
 } || {
     # Nighttime
-    [[ "$cast" =~ "Clear" ]] && cast=$(echo -n ) ||
-    [[ "$cast" =~ "Sunny" ]] && cast=$(echo -n ) ||
-    [[ "$cast" =~ "Partly cloudy" ]] && cast=$(echo -n ) ||
-    [[ "$cast" =~ "Cloudy" ]] && cast=$(echo -n ) ||
-    [[ "$cast" =~ "Shallow Fog" ]] && cast=$(echo -n )
+    [[ "$cast" =~ "Clear" ]] && cast=$(echo -n ) 
+    [[ "$cast" =~ "Sunny" ]] && cast=$(echo -n ) 
+    [[ "$cast" =~ "Partly cloudy" ]] && cast=$(echo -n ) 
+    [[ "$cast" =~ "Cloudy" ]] && cast=$(echo -n ) 
+    [[ "$cast" =~ "Shallow Fog" ]] && cast=$(echo -n ) 
 }
 [[ "$rain" == "0.0 mm" ]] && 
     rain=$(echo "") || 
